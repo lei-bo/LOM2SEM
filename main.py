14a4b7a6971c556e4116cdbd41ad115d2d88cf4a
Bo Lei <blei1@andrew.cmu.edu>
2022-02-27 17:56:40 -0500

    Implement Pix2Pix GAN training

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ import torch
 from torch import nn, optim
 from torch.utils.data import Dataset, DataLoader
 
-from models import UNet
-from utils import show_tensor_images
+from models import UNet, Discriminator
+from utils import show_tensor_images, AvgMeter, Recorder
 
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-N_EPOCHS = 100
-BATCH_SIZE = 4
+# modeling
+SIZE = (128, 128)
+LAMBDA_RECON = 100
+
+# training
+N_EPOCHS = 1000
+BATCH_SIZE = 16
 LR = 1e-4
-DISPLAY_STEP = 149
+
+# visualization and save
+DISPLAY_STEP = 500
+RECORD_STEP = 100
+NROW = 6 # number of images per row
 
 
 class ImagePairDataset(Dataset):
@@ -33,13 +42,14 @@ class ImagePairDataset(Dataset):
                                     dtype=str, delimiter='\n', ndmin=1)
         self.folder_A = f"{root}/{folder_A}"
         self.folder_B = f"{root}/{folder_B}"
-        self.transform = T.Compose([T.ToTensor()])
+        self.transform = T.Compose([T.Resize(SIZE),
+                                    T.ToTensor()])
 
     def __getitem__(self, index: int):
         img_name = self.img_names[index]
         img_A = self._get_image(self.folder_A, img_name)
         img_B = self._get_image(self.folder_B, img_name)
-        return img_A, img_B
+        return img_A, img_B[0][None,:,:]
 
     def _get_image(self, folder: str, img_name: str):
         img_path = f'{folder}/{img_name}'
@@ -53,29 +63,101 @@ class ImagePairDataset(Dataset):
         return len(self.img_names)
 
 def train():
-    train_set = ImagePairDataset("./data/mecs_steel", "LOM640", "SEM640", "all.txt")
+    train_set = ImagePairDataset("./data/mecs_steel",
+                                 "LOM640", "SEM640", "all.txt")
     train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    model = UNet(input_channels=3, output_channels=1, hidden_channels=32).to(DEVICE)
+    gen = UNet(input_channels=3, output_channels=1,
+               hidden_channels=32).to(DEVICE)
+    disc = Discriminator(input_channels=4).to(DEVICE)
     recon_criterion = nn.L1Loss().to(DEVICE)
-    optimizer = optim.Adam(model.parameters(), lr=LR)
+    adv_criterion = nn.BCEWithLogitsLoss().to(DEVICE)
+    gen_opt = optim.Adam(gen.parameters(), lr=LR)
+    disc_opt = optim.Adam(disc.parameters(), lr=LR)
 
     cur_step = 0
+    gen_meter = AvgMeter('gen_loss', ':.5f')
+    disc_meter = AvgMeter('disc_loss', ':.5f')
+    recorder = Recorder(('iter', 'gen_loss', 'disc_loss'))
     for epoch in range(N_EPOCHS):
         print(f"Epoch {epoch+1}/{N_EPOCHS}")
-        for imgs_A, imgs_B in tqdm(train_loader):
-            imgs_A, imgs_B = imgs_A.to(DEVICE), imgs_B.to(DEVICE)
-            preds_B = model(imgs_A)
-            optimizer.zero_grad()
-            loss = recon_criterion(preds_B, imgs_B[:,0,:,:][:,None,:,:])
-            loss.backward()
-            optimizer.step()
+        for real_A, real_B in tqdm(train_loader):
+            real_A, real_B = real_A.to(DEVICE), real_B.to(DEVICE)
+            fake_B = gen(real_A) # shape (N,1,H,W)
+
+            # Update discriminator
+            disc_opt.zero_grad()
+            disc_loss = get_disc_loss(disc, real_B, fake_B, real_A,
+                                      adv_criterion)
+            disc_meter.update(disc_loss.item(), BATCH_SIZE)
+            disc_loss.backward(retain_graph=True)
+            disc_opt.step()
+
+            # Update generator
+            gen_opt.zero_grad()
+            gen_loss = get_gen_loss(gen, disc, real_B, real_A,
+                                    adv_criterion, recon_criterion)
+            gen_meter.update(gen_loss.item(), BATCH_SIZE)
+            gen_loss.backward()
+            gen_opt.step()
 
             if cur_step % DISPLAY_STEP == 0:
-                print(f"Epoch {epoch+1}: Step {cur_step}: loss: {loss.item()}")
-                show_tensor_images(torch.cat([imgs_A, imgs_B,
-                                              torch.cat([preds_B]*3, dim=1)]),
-                                   nrow=BATCH_SIZE, num_images=BATCH_SIZE*3)
+                image_tensor = torch.cat([real_A[:NROW],
+                                          torch.cat([real_B]*3, dim=1)[:NROW],
+                                          torch.cat([fake_B]*3, dim=1)[:NROW]])
+                show_tensor_images(image_tensor, nrow=NROW, num_images=NROW*3)
+
+            if cur_step > 0 and cur_step % RECORD_STEP == 0:
+                print(f"Epoch {epoch + 1}: Step {cur_step}: "
+                      f"gen_loss: {gen_meter.avg}, "
+                      f"disc_loss: {disc_meter.avg}")
+                recorder.update([cur_step, gen_meter.avg, disc_meter.avg])
+                gen_meter.reset()
+                disc_meter.reset()
+                torch.save({'gen': gen.state_dict(),
+                            'gen_opt': gen_opt.state_dict(),
+                            'disc': disc.state_dict(),
+                            'disc_opt': disc_opt.state_dict(),
+                            'losses': recorder.record
+                            }, f"./checkpoints/pix2pix.pth")
             cur_step += 1
+
+def get_gen_loss(gen, disc, real, condition, adv_criterion, recon_criterion):
+    """
+    Get generator loss (adversarial loss + reconstruction loss).
+    Args:
+        gen: The generator model.
+        disc: The discriminator model.
+        real: Target real images.
+        condition: Source real images.
+        adv_criterion: The adversarial loss function.
+        recon_criterion:
+
+    Returns: Generator loss.
+    """
+    fake = gen(condition)
+    pred = disc(fake, condition)
+    gen_adv_loss = adv_criterion(pred, torch.ones_like(pred))
+    gen_recon_loss = recon_criterion(fake, real)
+    gen_loss = gen_adv_loss + LAMBDA_RECON * gen_recon_loss
+    return gen_loss
+
+def get_disc_loss(disc, real, fake, condition, adv_criterion):
+    """
+    Get discriminator loss.
+    Args:
+        disc: The discriminator model.
+        real: Target real images.
+        fake: Generated images.
+        condition: Source real images.
+        adv_criterion: The adversarial loss function.
+
+    Returns: Discriminator loss.
+    """
+    fake_pred = disc(fake.detach(), condition)
+    fake_loss = adv_criterion(fake_pred, torch.zeros_like(fake_pred))
+    real_pred = disc(real, condition)
+    real_loss = adv_criterion(real_pred, torch.ones_like(real_pred))
+    return real_loss + fake_loss
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -171,3 +171,34 @@ class UNet(nn.Module):
         xn = self.expand5(xn, x0)
         xn = self.downfeature(xn)
         return self.sigmoid(xn)
+
+
+class Discriminator(nn.Module):
+    '''
+    Discriminator Class
+    Structured like the contracting path of the U-Net, the discriminator will
+    output a matrix of values classifying corresponding portions of the image as real or fake.
+    Parameters:
+        input_channels: the number of image input channels
+        hidden_channels: the initial number of discriminator convolutional filters
+    '''
+    def __init__(self, input_channels, hidden_channels=8):
+        super(Discriminator, self).__init__()
+        self.upfeature = FeatureMapBlock(input_channels, hidden_channels)
+        self.contract1 = ContractingBlock(hidden_channels, use_bn=False)
+        self.contract2 = ContractingBlock(hidden_channels * 2)
+        self.contract3 = ContractingBlock(hidden_channels * 4)
+        self.contract4 = ContractingBlock(hidden_channels * 8)
+        #### START CODE HERE ####
+        self.final = nn.Conv2d(hidden_channels * 16, 1, kernel_size=1)
+        #### END CODE HERE ####
+
+    def forward(self, x, y):
+        x = torch.cat([x, y], dim=1)
+        x0 = self.upfeature(x)
+        x1 = self.contract1(x0)
+        x2 = self.contract2(x1)
+        x3 = self.contract3(x2)
+        x4 = self.contract4(x3)
+        xn = self.final(x4)
+        return xn
