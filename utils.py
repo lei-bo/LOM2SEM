a49b4c12ad6ea45c86c666a5a13646a78dcf4d02
Bo Lei <blei1@andrew.cmu.edu>
2022-02-27 17:33:46 -0500

    Add utility functions

--- utils.py
+++ utils.py
@@ -15,3 +15,36 @@ def show_tensor_images(image_tensor, num_images=8, nrow=4):
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
     plt.axis("off")
     plt.show()
+
+
+class AvgMeter(object):
+    """Computes and stores the average and current value."""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+class Recorder(object):
+    """Record lists of variables"""
+    def __init__(self, names):
+        self.names = names
+        self.record = {}
+        for name in self.names:
+            self.record[name] = []
+
+    def update(self, vals):
+        for name, val in zip(self.names, vals):
+            self.record[name].append(val)
